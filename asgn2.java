557b58411d3de68056d099eb6e8be6254133c4e2
Manish Godbole <116292202+manish0222@users.noreply.github.com>
2023-08-04 10:47:47 +0530

    Updated asgn2.java

--- asgn2.java
+++ asgn2.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class asgn2 {
     public static void main(String[] args) {
         String filename = "input.txt"; // Replace 'input.txt' with the actual file path
@@ -32,60 +33,123 @@ public class asgn2 {
             DecSt.put("dc", 1);
             DecSt.put("ds", 2);
         };
+        HashMap<String,Integer> Reg=new HashMap<>();
+        {
+            Reg.put("areg", 1);
+            Reg.put("breg", 2);
+        };
         ArrayList<ArrayList<String>> PassOne = new ArrayList<>();
         HashMap<String,Integer> SymbolTab=new HashMap<>();
+        HashMap<String,Integer> LitTab=new HashMap<>();
+        int loc=0;
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             StringBuilder sentence = new StringBuilder();
             while ((line = br.readLine()) != null) {
-                int count=0;
+                System.out.print(loc+" ");
                 String[] words = line.split("\\s+");
                 ArrayList<String> lines = new ArrayList<>();
-                for (String word : words) {
-                    if(word.contains(":")){
-                        String[] wordsep = word.split(":");
+                for (int i=0;i< words.length;i++) {
+                    if(words[i].contains(":")){
+                        String[] wordsep = words[i].split(":");
                         String firstWord = wordsep[0].trim();
-                        String secondWord = wordsep[1].trim();
+                        // String secondWord = wordsep[1].trim();
                         lines.add(firstWord);
-                        lines.add(secondWord);
+                        SymbolTab.put(firstWord, SymbolTab.size()+1);
                     }
-                    else if(word.contains(",")){
-                        String[] wordsep = word.split(",");
+                    else if(words[i].contains(",")){
+                        String[] wordsep = words[i].split(",");
                         String firstWord = wordsep[0].trim();
                         String secondWord = wordsep[1].trim();
                         lines.add(firstWord);
                         lines.add(secondWord);
+                        if(secondWord.contains("=")){
+                            LitTab.put(secondWord, LitTab.size()+1);
+                        }
+                        
+                    }
+                    else if(words[i].equals("start")){
+                        lines.add(words[i]);
+                        loc=loc+(Integer.parseInt(words[i+1]));
+                        // break;
                     }
                     else{
-                        lines.add(word);
+                        lines.add(words[i]);
                     }
-                } 
+                }
                 PassOne.add(lines);
+                loc++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int loc=0;
+        System.out.println("\n");
         for (ArrayList<String> innerList : PassOne) {
-            System.out.print(loc+" ");
+            for (int i=0;i< innerList.size();i++) {
+                String word=innerList.get(i);
+                System.out.print(word+" ");
+            }System.out.println();
+        }
+        loc=0;
+        System.out.println("\n");
+        for (ArrayList<String> innerList : PassOne) {
+            System.out.print(loc+"  ");
             for (int i=0;i< innerList.size();i++) {
                 String word=innerList.get(i);
                 if(ImpSt.containsKey(word)){
-
+                    System.out.print("   IS,"+ImpSt.get(word));
                 }
+
+
                 else if(AssDir.containsKey(word)){
-                    if(word.toLowerCase()=="start"){
-                        loc=Integer.parseInt(innerList.get(i+1));
+                    System.out.print("   AS,"+AssDir.get(word));
+                    if(word.equals("start")){
+                        String lc=innerList.get(i+1);
+                        int k=Integer.parseInt(lc);
+                        loc=--k;
+                        break;
                     }
-                    else if(word.toLowerCase()=="origin"){
-                        loc=Integer.parseInt(innerList.get(i+1));
+                    else if(word.equals("end")){
+                        // loc=Integer.parseInt(innerList.get(i+1));
                     }
+                }
+
+                else if(DecSt.containsKey(word)){
+                    System.out.print("   DS,"+DecSt.get(word));
+                    if(word.equals("ds")){
+                        String lc=innerList.get(i+1);
+                        int k=Integer.parseInt(lc);
+                        loc+=k;
+                        loc--;
+                        // break;
+                    }
+                }
+                else if(LitTab.containsKey(word)){
+                    System.out.print("   LT,"+LitTab.get(word));
+                }
+                else if(SymbolTab.containsKey(word) && i!=0){
+                    System.out.print("   ST,"+SymbolTab.get(word));
+                }
+                else if(SymbolTab.containsKey(word) && i==0){
+                    // System.out.print("   ST,"+SymbolTab.get(word));
+                    SymbolTab.put(word,loc);
+                }
+                else if(Reg.containsKey(word)){
+                    System.out.print("   "+word+","+Reg.get(word));
+                }
+
+                else{
+                    System.out.print("   "+word+",");
                 }
             }
             loc++;
             System.out.println();
         }
-
+        System.out.println("\nSymbol Table");
+        for (String key : SymbolTab.keySet()) {
+            int value = SymbolTab.get(key);
+            System.out.println(key + " -> " + value);
+        }
         
     }
 }
